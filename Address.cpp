3d80a2befe09144fcc1875595ad81674bb3b4cbd
Artyom <kolmazeff@yandex.ru>
2025-01-15 02:27:41 +0300

    First commit

---
+++ Address.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+
+struct Adress
+{
+    std::string City;
+    std::string Street;
+    int NumberHome;
+    int NumberApartment;
+    int Index;
+};
+
+void InputStructer(Adress* ad) {
+   
+    std::cout << "Город: " << ad->City << "\n" << "Улица: " << ad->Street << "\n" << "Номер дома: " << ad->NumberHome << "\n" << "Номер квартиры:" << ad->NumberApartment << "\n" << "Индекс: " << ad->Index << std::endl;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    Adress a;
+    Adress* p_ad = &a;
+    std::cin >> p_ad->City;
+    std::cin >> p_ad->Street;
+    std::cin >> p_ad->NumberHome;
+    std::cin >> p_ad->NumberApartment;
+    std::cin >> p_ad->Index;
+
+    InputStructer(p_ad);
+}
+
+
