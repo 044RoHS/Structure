6b5bf834c63c2c7308e1fd3e772785f409f6020e
Artyom <kolmazeff@yandex.ru>
2025-01-15 01:32:28 +0300

    First commit

---
+++ Cash.cpp
@@ -0,0 +1,36 @@
+﻿
+
+#include <iostream>
+
+struct Bank
+{
+    int NUmberczet;
+    std::string Name;
+    double Money;
+
+}; 
+
+int invoiceBank() 
+{
+    Bank bank;
+    Bank* P_bank = &bank;
+    std::cout << "Введите номер счёта: ";
+    std::cin >> P_bank->NUmberczet;
+    std::cout << "Введите имя владельца: ";
+    std::cin >> P_bank->Name;
+    std::cout << "Введите баланс: ";
+    std::cin >> P_bank->Money;
+    std::cout << "Введите новый баланс: ";
+    std::cin >> P_bank->Money;
+    std::cout << "Ваш счёт: " << P_bank->NUmberczet << ", " << P_bank->Name << ", " << P_bank->Money;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    invoiceBank();
+  
+
+}
+
