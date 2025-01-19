378c9ba62a5c7c7ad00f0fb339a60371ed7e7ce7
Artyom <kolmazeff@yandex.ru>
2025-01-19 04:15:45 +0300

    Second commit

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -4,94 +4,88 @@
  
 enum class Season
 {
-    Январь = 1,
-    Февраль,
-    Март,
-    Апрель,
-    Май,
-    Июнь,
-    Июль,
-    Август,
-    Сентабрь,
-    Октябрь,
-    Ноябрь,
-    Декабрь
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
 };
   
 int main()
 {
-    int NumberMonth;
+    int input = 0;
+    const int end_input = 0;
     setlocale(LC_ALL, "RU");
-   
 
-    while (1) 
+
+    while (true)
     {
-        std::cout << "Введите номер месяца:" << " ";
-        std::cin >> NumberMonth;
-        if(NumberMonth != 0)
+        std::cout << "Введите номер месяца или 0 для выхода: ";
+        std::cin >> input;
+        if (input > static_cast<int>(Season::December))
         {
-            switch (NumberMonth)
-            {
-            case static_cast<int>(Season::Январь):
-                std::cout << "Январь" << std::endl;
-                break;
-            case static_cast<int>(Season::Февраль):
-                std::cout << "Февраль" << std::endl;
-                break;
-            case static_cast<int>(Season::Март):
-                std::cout << "Март" << std::endl;
-                break;
-            case static_cast<int>(Season::Апрель):
-                std::cout << "Апрель" << std::endl;
-                break;
-            case static_cast<int>(Season::Май):
-                std::cout << "Май" << std::endl;
-                break;
-            case static_cast<int>(Season::Июнь):
-                std::cout << "Июнь" << std::endl;
-                break;
-            case static_cast<int>(Season::Июль):
-                std::cout << "Июль" << std::endl;
-                break;
-            case static_cast<int>(Season::Август):
-                std::cout << "Август" << std::endl;
-                break;
-            case static_cast<int>(Season::Сентабрь):
-                std::cout << "Сентабрь" << std::endl;
-                break;
-            case static_cast<int>(Season::Октябрь):
-                std::cout << "Октябрь" << std::endl;
-                break;
-            case static_cast<int>(Season::Ноябрь):
-                std::cout << "Ноябрь" << std::endl;
-                break;
-            case static_cast<int>(Season::Декабрь):
-                std::cout << "Декабрь" << std::endl;
-                break;
-            default:
-                std::cout << "Неправильный номер!" << std::endl;
-
-            }
+            std::cout << "Месяца с таким номером не существует!" << std::endl;
+            continue;
         }
-        else 
+        else if (input == end_input)
         {
-            std::cout << "До свидания " << std::endl;
+            std::cout << "До встречи!" << std::endl;
             break;
         }
-            
-        
-        
-       
-      
+        Season m = static_cast<Season>(input);
+
+        switch (m)
+        {
+        case (Season::January):
+            std::cout << "Январь" << std::endl;
+            break;
+        case (Season::February):
+            std::cout << "Февраль" << std::endl;
+            break;
+        case (Season::March):
+            std::cout << "Март" << std::endl;
+            break;
+        case (Season::April):
+            std::cout << "Апрель" << std::endl;
+            break;
+        case (Season::May):
+            std::cout << "Май" << std::endl;
+            break;
+        case (Season::June):
+            std::cout << "Июнь" << std::endl;
+            break;
+        case (Season::July):
+            std::cout << "Июль" << std::endl;
+            break;
+        case (Season::August):
+            std::cout << "Август" << std::endl;
+            break;
+        case (Season::September):
+            std::cout << "Сентабрь" << std::endl;
+            break;
+        case (Season::October):
+            std::cout << "Октябрь" << std::endl;
+            break;
+        case (Season::November):
+            std::cout << "Ноябрь" << std::endl;
+            break;
+        case(Season::December):
+            std::cout << "Декабрь" << std::endl;
+            break;
+
+        }
+
     }
-    
-
-
-
-
-
-
 }
+
+    
 
     
 
